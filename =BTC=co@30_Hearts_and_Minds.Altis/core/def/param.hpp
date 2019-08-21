2d6f5da88e11fca951233feeff919bf48bee7db8
Gold John King <goldjohnking@live.cn>
2019-08-21 21:12:57 +0800

    Parameter Tweak
    Parameter Tweak:
    Decrease AI weapon sway skill.

--- =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
@@ -219,7 +219,7 @@ class Params {
         title = __EVAL(format ["      %1, %2", localize "STR_A3_CFGVEHICLES_MODULESKILL_F_0", localize "STR_BTC_HAM_PARAM_SKILL_AIMSKILL"]);
         values[]={0,1,2,3,4,5,6,7,8,9,10};
         texts[]={"0","0.1","0.2","0.3","0.4","0.5","0.6","0.7","0.8","0.9","1"};
-        default = 1;
+        default = 0;
     };
     class btc_p_set_skill_aimingSpeed { // Set skill, aimingSpeed
         title = __EVAL(format ["      %1, %2", localize "STR_A3_CFGVEHICLES_MODULESKILL_F_0", localize "STR_BTC_HAM_PARAM_SKILL_AIMSPEED"]);
