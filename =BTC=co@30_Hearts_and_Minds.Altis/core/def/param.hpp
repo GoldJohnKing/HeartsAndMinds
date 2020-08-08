8d26c8e84f44995c84579a183572dfb2df56bbec
GoldJohnKing <GoldJohnKing@Live.cn>
2020-08-08 11:21:18 +0800

    Change and Tweak.
    Change: Add three custom location.
    Tweak: Reputation at start set to very low.

--- =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
@@ -369,7 +369,7 @@ class Params {
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_OTHER_REPSTART"]);
         values[]={0, 200, 500, 750};
         texts[]={$STR_ACE_ViewDistance_object_verylow,$STR_A3_TIME_TRIALS_MISCTT_ROF_25,$STR_3DEN_ATTRIBUTES_SPEEDMODE_NORMAL,$STR_A3_TIME_TRIALS_MISCTT_ROF_21}; //texts[]={"Very Low","Low","Normal","High"};
-        default = 500;
+        default = 0;
     };
     class btc_p_rep_notify { // Show reputation change:
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_OTHER_SHOWREP"]);
