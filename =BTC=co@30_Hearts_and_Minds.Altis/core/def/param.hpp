d22d2afcd3cd74b237e0bfcee136d4792a15acc4
GoldJohnKing <GoldJohnKing@Live.cn>
2020-09-26 10:37:22 +0800

    Tweak: Disable H&M headless client function and use ACEX one
    Tweak: Disable H&M headless client function and use ACEX one

--- =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
@@ -393,7 +393,7 @@ class Params {
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_OTHER_AUTOHEADLESS"]);
         values[]={0,1};
         texts[]={$STR_DISABLED,$STR_ENABLED};
-        default = 1;
+        default = 0;
     };
     class btc_p_debug { // Debug:
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_OTHER_DEBUG"]);
